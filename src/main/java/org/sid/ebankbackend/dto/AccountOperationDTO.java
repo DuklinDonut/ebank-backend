5fd41f7b9c2fbf907dae4e008bd9c96da30bd5e0
DuklinDonut <majidi_fatimaezzahra@emsi-edu.ma>
2022-06-24 16:56:37 +0000

    new version

---
+++ src/main/java/org/sid/ebankbackend/dto/AccountOperationDTO.java
@@ -0,0 +1,14 @@
+package org.sid.ebankbackend.dto;
+
+import lombok.Data;
+import org.sid.ebankbackend.enums.OperationType;
+
+import java.util.Date;
+@Data
+public class AccountOperationDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+}
